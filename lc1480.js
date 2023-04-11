b8025dc1899ae4e3a37ddfbc6342ef62cb523a1e
vincentYoung <vinni.xander@gmail.com>
2023-04-11 16:58:55 -0600

    refactor lc1480 using reduce

--- lc1480.js
+++ lc1480.js
@@ -1,22 +1,32 @@
+// const runningSum = function (nums) {
+//   // if nums only has one value return nums
+//   if (nums.length === 1)  {
+//     return nums
+//   }
+//   // else 
+//   // variable for curr running sum;
+//   let runningSum = 0
+//   // variable for new arr
+//   const newArr = []
+//   // loop over nums
+//   for (let num of nums) {
+//     // calculate new running sum
+//     runningSum += num
+//     // push to new arr
+//     newArr.push(runningSum);
+//   }
+//   return newArr
+// };
+
 const runningSum = function (nums) {
-  // if nums only has one value return nums
-  if (nums.length === 1)  {
-    return nums
-  }
-  // else 
-  // variable for curr running sum;
-  let runningSum = 0
-  // variable for new arr
-  const newArr = []
-  // loop over nums
-  for (let num of nums) {
-    // calculate new running sum
-    runningSum += num
-    // push to new arr
-    newArr.push(runningSum);
-  }
-  return newArr
-};
+  return nums.reduce((newArr, curr, index) => {
+    if (index > 0) {
+      newArr.push(curr + newArr[index - 1])
+    }
+    return newArr
+  },[nums[0]])
+}
+
 
 console.log(`
   Input: [1,2,3,4],
